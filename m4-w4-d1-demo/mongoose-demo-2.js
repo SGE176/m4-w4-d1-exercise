318373b7179f33c27ef1c2b45b9a147f2ab932ea
SGE176 <s.ge.176@westcliff.edu>
2022-04-18 20:35:43 -0500

    commit

--- m4-w4-d1-demo/mongoose-demo-2.js
+++ m4-w4-d1-demo/mongoose-demo-2.js
@@ -1,3 +1,4 @@
+const { GridFSBucketReadStream } = require('mongodb');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -5,6 +6,19 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
+  const kittySchema = new mongoose.Schema({
+    name: String
+  });
+  kittySchema.methods.speak = function(){
+    const gretting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+  }
+  const Kitten = mongoose.model('Kitten', kittySchema);
+
+  const fluffy = new Kitten({ name: 'fluffy' });
+  fluffy.speak(); // "Meow name is fluffy"
 });
 
 
